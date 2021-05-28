e6af64723e41bedfc5bdecc57b4c021779c4e8ac
heningm <13699448253@163.com>
2021-05-28 23:19:49 +0800

    2021-05-28 leetcode

---
+++ src/main/java/zijie/leet23合并k个链表.java
@@ -0,0 +1,28 @@
+package zijie;
+
+
+public class leet23合并k个链表 {
+    public static void main(String[] args) {
+        //测试代码  
+        Solution solution = new leet23合并k个链表().new Solution();
+
+    }
+
+
+    /**
+     * 23. 合并k个链表
+     */
+    class Solution {
+
+    }
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+}
