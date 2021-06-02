780f9984265d33074b889799b197a92887096c0e
heningm <13699448253@163.com>
2021-06-03 00:22:34 +0800

    2021-06-3 刷题记录

--- src/main/java/zijie/leet128最长连续序列.java
+++ src/main/java/zijie/leet128最长连续序列.java
@@ -1,7 +1,7 @@
 package zijie;
 
 
-import java.util.Arrays;
+import java.util.*;
 
 public class leet128最长连续序列 {
     public static void main(String[] args) {
@@ -18,7 +18,47 @@ public class leet128最长连续序列 {
      */
     class Solution {
         public int longestConsecutive(int[] nums) {
-            return 0;
+            //return extracted(nums);
+            // 外层循环剪枝
+            Set<Integer> set = new HashSet<>();
+            for (int num : nums) {
+                set.add(num);
+            }
+            int longest = 0;
+            for (int num : nums) {
+                // 不存在num - 1 在判断
+                // 如果存在num - 1，那么之后找到num -1 这个数字后，一定会到当前这个数字，不用这个时候进行判断
+                // 如果不存在 num - 1 说明当前这个数就是这个最长连续序列的开头
+                if (!set.contains(num - 1)) {
+                    int curlength = 1;
+                    while (set.contains(num + 1)) {
+                        curlength ++;
+                        num ++;
+                    }
+                    longest = Math.max(longest, curlength);
+                }
+            }
+            return longest;
+        }
+
+        // 枚举法
+        // 会超时，一直枚举 是否存在数等于 x + 1
+        private int extracted(int[] nums) {
+            Set<Integer> set = new HashSet<>();
+            int longest = 0;
+            // 去重
+            for (int num : nums) {
+                set.add(num);
+            }
+            for (Integer integer : set) {
+                int currLength = 0;
+                while (set.contains(integer)) {
+                    currLength++;
+                    integer++;
+                }
+                longest = Math.max(longest, currLength);
+            }
+            return longest;
         }
     }
 }
